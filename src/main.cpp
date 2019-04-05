a8f5afaa6752da913afab9a23d7527eed831e489
Tim Süberkrüb <dev@timsueberkrueb.io>
2019-04-05 21:40:10 +0200

    Remove now broken scaling workaround

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,11 @@
 #include <QTranslator>
 #include <QDebug>
 
-#ifdef UBUNTU_TOUCH_SUPPORT
-#include <QByteArray>
-#include <QString>
-
-#define ENV_GRID_UNIT_PX "GRID_UNIT_PX"
-#define DEFAULT_GRID_UNIT_PX 8
-
-void fixHighDpiUbuntuTouch() {
-    // Qt's builtin High Dpi support does not work on Ubuntu touch, yet.
-    // Therefore we workaround by manually setting QT_SCALE_FACTOR.
-    int gridUnitPixel = qgetenv(ENV_GRID_UNIT_PX).toInt();
-    double scaleFactor = (double)gridUnitPixel / DEFAULT_GRID_UNIT_PX;
-    qputenv("QT_SCALE_FACTOR", QString::number(scaleFactor).toUtf8());
-}
-#endif
 
 int main(int argc, char *argv[])
 {
     // Enabled high dpi scaling
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
-#ifdef UBUNTU_TOUCH_SUPPORT
-    fixHighDpiUbuntuTouch();
-#endif
 
     // Set Material Design QtQuick Controls 2 style
     qputenv("QT_QUICK_CONTROLS_STYLE", "material");
