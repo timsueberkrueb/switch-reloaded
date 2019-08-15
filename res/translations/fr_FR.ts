7e809cab5f5b18ba67c89c008be4928d5229b447
Anne017 <anneonyme017@netcourrier.com>
2019-08-15 19:26:43 +0200

    french translation added

---
+++ res/translations/fr_FR.ts
@@ -0,0 +1,334 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <source>A simple game about linear equations</source>
+        <translation>Un jeu simple comportant des équations linéaires</translation>
+    </message>
+    <message>
+        <source>Switch Reloaded is a platform independent successor of the popular &lt;a href=&quot;%1&quot;&gt;Ubuntu Phone game &quot;Switch&quot;&lt;/a&gt;.</source>
+        <translation>Switch Reloaded est un successeur indépendant de la platforme du populaire &lt;a href=&quot;%1&quot;&gt;jeu sur Ubuntu Phone &quot;Switch&quot;&lt;/a&gt;.</translation>
+    </message>
+    <message>
+        <source>Credits</source>
+        <translation>Remerciements</translation>
+    </message>
+    <message>
+        <source>Many thanks to &lt;a href=&apos;%1&apos;&gt;Andrew Penkrat&lt;/a&gt; for his help and contribution ensuring Switch generates systems with one and only one solution 😉.</source>
+        <translation>Merci beaucoup à &lt;a href=&apos;%1&apos;&gt;Andrew Penkrat&lt;/a&gt; pour son aide et sescontributions afin de s'assurer que Switch génère des systèmes avec une seule et unique solution 😉.</translation>
+    </message>
+    <message>
+        <source>Thanks to &lt;a href=&apos;%1&apos;&gt;Stuart Langridge&lt;/a&gt; for testing, design input and QA of Switch for Ubuntu Phone.</source>
+        <translation>Merci à &lt;a href=&apos;%1&apos;&gt;Stuart Langridge&lt;/a&gt; pour les tests, la conception et l'assurance qualité de Switch sur Ubuntu Phone.</translation>
+    </message>
+    <message>
+        <source>License and Copyright</source>
+        <translation>Licence et copyright</translation>
+    </message>
+    <message>
+        <source>Source code available on &lt;a href=&apos;%1&apos;&gt;GitHub&lt;/a&gt;.</source>
+        <translation>Le code source est disponible sur &lt;a href=&apos;%1&apos;&gt;GitHub&lt;/a&gt;.</translation>
+    </message>
+    <message>
+        <source>Rate</source>
+        <translation>Évaluer</translation>
+    </message>
+    <message>
+        <source>Report bug</source>
+        <translation>Signaler un problème</translation>
+    </message>
+    <message>
+        <source>Contribute</source>
+        <translation>Contribuer</translation>
+    </message>
+    <message>
+        <source>Switch Reloaded contains icons from the &lt;a href=&apos;%1&apos;&gt;Ionicons icon set&lt;/a&gt; which are licensed under the &lt;a href=&apos;%2&apos;&gt;MIT license&lt;/a&gt;.</source>
+        <translation>Switch Reloaded contient les icônes du &lt;a href=&apos;%1&apos;&gt;jeu d'icônes Ionicons&lt;/a&gt; qui sont sous  &lt;a href=&apos;%2&apos;&gt;licence du MIT&lt;/a&gt;.</translation>
+    </message>
+</context>
+<context>
+    <name>LevelModel</name>
+    <message>
+        <source>Spoon boy</source>
+        <translation>Le garçon à la cuillère</translation>
+    </message>
+    <message>
+        <source>Mouse</source>
+        <translation>La souris</translation>
+    </message>
+    <message>
+        <source>Choi</source>
+        <translation>Choi</translation>
+    </message>
+    <message>
+        <source>Trinity</source>
+        <translation>Trinity</translation>
+    </message>
+    <message>
+        <source>Cypher</source>
+        <translation>Cypher</translation>
+    </message>
+    <message>
+        <source>Switch</source>
+        <translation>Switch</translation>
+    </message>
+    <message>
+        <source>Morpheus</source>
+        <translation>Morpheus</translation>
+    </message>
+    <message>
+        <source>Agent Smith</source>
+        <translation>L'agent Smith</translation>
+    </message>
+    <message>
+        <source>Neo</source>
+        <translation>Néo</translation>
+    </message>
+    <message>
+        <source>Merovingian</source>
+        <translation>Le Mérovingien</translation>
+    </message>
+    <message>
+        <source>Oracle</source>
+        <translation>L'Oracle</translation>
+    </message>
+    <message>
+        <source>The Architect</source>
+        <translation>L'Architecte</translation>
+    </message>
+</context>
+<context>
+    <name>LevelScreen</name>
+    <message>
+        <source>Generate new</source>
+        <translation>Regénérer</translation>
+    </message>
+    <message>
+        <source>Matrix solved.</source>
+        <translation>Matrice résolue.</translation>
+    </message>
+</context>
+<context>
+    <name>LevelSelection</name>
+    <message>
+        <source>Tutorial</source>
+        <translation>Tutoriel</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <source>Locked</source>
+        <translation>Verrouillé</translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <source>Level %1 unlocked</source>
+        <translation>Niveau %1 déverrouillé></translation>
+    </message>
+</context>
+<context>
+    <name>TutorialPage</name>
+    <message>
+        <source>Tutorial</source>
+        <translation>Tutoriel</translation>
+    </message>
+    <message>
+        <source>Hey! Welcome to Switch.</source>
+        <translation>Salut ! Bienvenue dans Switch.</translation>
+    </message>
+    <message>
+        <source>Switch is a game about solving systems of linear equations using the Gaussian elimination of matrices.</source>
+        <translation>Switch est un jeu qui consiste à résoudre des systèmes d'équations linéaires en utilisant l'élimination gaussienne des matrices.</translation>
+    </message>
+    <message>
+        <source>Yeah, I know. That sounds scary. It doesn&apos;t really sound like a lot of fun, rather like one of &lt;i&gt;those&lt;/i&gt; maths lessons, right? 😉</source>
+        <translation>Oui, je sais. Cela semble effrayant. Cela n'a pas l'air d'être une partie de plaisir, plutôt un de &lt;i&gt;ces&lt;/i&gt; cours de maths, n'est-ce pas ? 😉</translation>
+    </message>
+    <message>
+        <source>Then let us hope that I was wrong</source>
+        <translation>Alors espérons que j'ai tort</translation>
+    </message>
+    <message>
+        <source>Morpheus</source>
+        <translation>Morpheus</translation>
+    </message>
+    <message>
+        <source>What is the matrix?</source>
+        <translation>Qu'est-ce que la Matrice ?</translation>
+    </message>
+    <message>
+        <source>The answer is out there, Neo. It&apos;s looking for you, and it will find you if you want it to.</source>
+        <translation>La réponse est dehors Néo. Elle te cherche et elle te trouvera si tu le souhaites.</translation>
+    </message>
+    <message>
+        <source>Trinity</source>
+        <translation>Trinity</translation>
+    </message>
+    <message>
+        <source>Now we&apos;re removing all the clutter ...</source>
+        <translation>Maintenant, nous retirons tous le bazar...</translation>
+    </message>
+    <message>
+        <source>... and we get a matrix form like the one above.</source>
+        <translation>...et nous obtenons une forme de matrice comme celle ci-dessus.</translation>
+    </message>
+    <message>
+        <source>Solution?</source>
+        <translation>Solution ?</translation>
+    </message>
+    <message>
+        <source>In Switch, all matrices have exactly one correct solution.</source>
+        <translation>Dans Switch, toutes les matrices ont une seule et unique solution.</translation>
+    </message>
+    <message>
+        <source>A solution is an assignment of values to the variables (x, y) such that all equations are correct.</source>
+        <translation>Une solution est une affectation de valeurs aux variables (x, y) afin que toutes les équations soient correctes.</translation>
+    </message>
+    <message>
+        <source>In our case the solution x=2, y=1 satisfies the linear system.</source>
+        <translation>Dans notre cas, la solution x=2, y=1 satisfait le système linéaire.</translation>
+    </message>
+    <message>
+        <source>How to solve a matrix</source>
+        <translation>Comment résoudre une matrice ?</translation>
+    </message>
+    <message>
+        <source>There are multiple ways to solve a system of linear equations.</source>
+        <translation>Il y a plusieurs manières de résoudre un système d'équations linéaires.</translation>
+    </message>
+    <message>
+        <source>Dans Switch, vous utiliserez l'&lt;a href=&apos;https://fr.wikipedia.org/wiki/Élimination_de_Gauss-Jordan&apos;&gt;élimination de Gauss-Jordan&lt;/a&gt;, aussi connue sous le nom de méthode du pivot de Gauss.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Using the Gaussian elimination algorithm you can perform several actions on a matrix of coefficients.</source>
+        <translation>En utilisant la méthode du pivot de Gauss, vous pouvez effectuer plusieurs actions sur une matrice de coefficients.</translation>
+    </message>
+    <message>
+        <source>Those actions are called row operations. In Switch you are able to ...</source>
+        <translation>Ces actions sont appelées opérations élémentaires sur les lignes. Dans Switch, vous pouvez...</translation>
+    </message>
+    <message>
+        <source>... multiply a row by a number</source>
+        <translation>...multiplier une ligne par un nombre</translation>
+    </message>
+    <message>
+        <source>... add one row to another</source>
+        <translation>...ajouter une ligne à une autre</translation>
+    </message>
+    <message>
+        <source>The goal of all this is to eliminate variables by setting the coefficient to zero. Remember our example:</source>
+        <translation>Le but de tout cela est d'éliminer les variables en rendant le coefficient égal à zéro. Souvenez-vous de notre exemple :</translation>
+    </message>
+    <message>
+        <source>To solve this matrix, we would like to eliminate the x variable from the second row.</source>
+        <translation>Pour résoudre cette matrice, nous aimerions éliminer la variable x de la deuxième ligne.</translation>
+    </message>
+    <message>
+        <source>To achieve this, we&apos;ll multiply the first row by -1 and add it to the second row. The result:</source>
+        <translation>Pour cela, nous allons multiplier la première ligne par -1 et l'ajouter à la seconde ligne. Le résultat :</translation>
+    </message>
+    <message>
+        <source>In this simple case, this was the only step needed to solve the system.</source>
+        <translation>Dans ce cas simple, c'était la seule étape nécessaire pour résoudre le système.</translation>
+    </message>
+    <message>
+        <source>Our matrix is transformed into upper &lt;a href=&apos;https://en.wikipedia.org/wiki/Triangular_matrix&apos;&gt;triangular form&lt;/a&gt;, which means that all matix entries below the &lt;a href=&apos;https://en.wikipedia.org/wiki/Main_diagonal&apos;&gt;main diagonal&lt;/a&gt; are zero.</source>
+        <translation>Notre matrice est transformée en &lt;a href=&apos;https://fr.wikipedia.org/wiki/Matrice_triangulaire&apos;&gt;forme triangulaire supérieure&lt;/a&gt;, ce qui signifie que toutes les entrées de la matrice sous la &lt;a href=&apos;https://fr.wikipedia.org/wiki/Diagonale_principale&apos;&gt;diaonale principale&lt;/a&gt; sont nulles.</translation>
+    </message>
+    <message>
+        <source>We can now finish solving the matrix in your head:</source>
+        <translation>Nous pouvons maintenant finir de résoudre la matrice de tête :</translation>
+    </message>
+    <message>
+        <source>Therefore: x = 2, y = 1</source>
+        <translation>D'où : x=2, y=1</translation>
+    </message>
+    <message>
+        <source>Alternatively we can continue to simplify the matrix using the row operations ...</source>
+        <translation>Sinon, nous pouvons continuer de simplifier la matrice en utilisant les opérations élémentaires sur les lignes...</translation>
+    </message>
+    <message>
+        <source> ... until we end up with the so called reduced &lt;a href=&apos;https://en.wikipedia.org/wiki/Row_echelon_form&apos;&gt;row echelon form&lt;/a&gt;.</source>
+        <translation> ...jusqu'à ce que nous atteignons ce qu'on appelle la &lt;a href=&apos;https://fr.wikipedia.org/wiki/Matrice_%C3%A9chelonn%C3%A9e&apos;&gt;matrice échelonnée réduite&lt;/a&gt;.</translation>
+    </message>
+    <message>
+        <source>This means that in every row there is exactly one leading coefficient which is 1.</source>
+        <translation>Cela signifie qu'il y a exactement un coefficient pivot égal à 1 dans chaque ligne.</translation>
+    </message>
+    <message>
+        <source>All other entries in the same column are 0.</source>
+        <translation>Toutes les autres entrées dans la même colonne sont égales à zéro.</translation>
+    </message>
+    <message>
+        <source>And ... action</source>
+        <translation>Et... action</translation>
+    </message>
+    <message>
+        <source>In Switch, this all is even more hassle-free. I think it&apos;s time to try it yourself.</source>
+        <translation>Dans Switch, tout cela est encore plus aisé. Je pense qu'il est temps d'essayer vous-même.</translation>
+    </message>
+    <message>
+        <source>Row operations:</source>
+        <translation>Opérations en ligne :</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>Ajouter</translation>
+    </message>
+    <message>
+        <source>• You can drag and drop a row onto another to add it</source>
+        <translation>• Vous pouvez faire un glisser / déposer d'une ligne sur une autre pour l'ajouter</translation>
+    </message>
+    <message>
+        <source>Multiply</source>
+        <translation>Multiplier</translation>
+    </message>
+    <message>
+        <source>• Tap on a row to select it</source>
+        <translation>• Appuyez sur une ligne pour la sélectionner</translation>
+    </message>
+    <message>
+        <source>• The multiply slider will show up</source>
+        <translation>• Le facteur de multiplication apparaîtra</translation>
+    </message>
+    <message>
+        <source>• Hold and move the slider increase/decrease the value of the mutliplicator, release to multiply</source>
+        <translation>• Maintenir appuyé le curseur et le déplacer augmente / diminue la valeur du facteur de multiplication, relâchez-le pour multiplier</translation>
+    </message>
+    <message>
+        <source>Simplify the matrix as much as possible by using the row operations.</source>
+        <translation>Simplifiez autant que possible la matrice en utilisant les opérations élémentaires sur les lignes.</translation>
+    </message>
+    <message>
+        <source>You will end up with the so called reduced &lt;a href=&apos;https://en.wikipedia.org/wiki/Row_echelon_form&apos;&gt;row echelon form&lt;/a&gt; and Switch will detect that the matrix is solved.</source>
+        <translation>Vous finirez avec ce que l'on appelle la &lt;a href=&apos;https://fr.wikipedia.org/wiki/Matrice_%C3%A9chelonn%C3%A9e&apos;&gt;matrice échelonnée réduite&lt;/a&gt; et Switch détectera que la matrice est résolue.</translation>
+    </message>
+    <message>
+        <source>Let&apos;s go!</source>
+        <translation>Allons-y</translation>
+    </message>
+    <message>
+        <source>The problem is choice.</source>
+        <translation>Le problème, c'est le choix.</translation>
+    </message>
+    <message>
+        <source>Neo</source>
+        <translation>Néo</translation>
+    </message>
+    <message>
+        <source>The matrices in Switch are matrices of coefficients of associated systems of linear equations. Consider the following linear system:</source>
+        <translation>Les matrices dans Switch sont des matrices de coefficients de systèmes associés d'équations linéaires. Considérons le système linéaire suivant : </translation>
+    </message>
+</context>
+</TS>
